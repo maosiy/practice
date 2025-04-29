8d111545c9290f9449e0980ed32972f142cc0f3e
siyu.mao <siyu.mao@thundersoft.com>
2025-04-29 18:10:50 +0800

    the second change

--- text.cpp
+++ text.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 int main() 
 {    
-    std::cout << "Hello Git!123456789" << std::endl;    
+    std::cout << "Hello Git!123456789" << std::endl;
+    std::cout << "666!!!" << std::endl;
     return 0;}
