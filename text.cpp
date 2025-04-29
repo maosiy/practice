5bfbf83156975429a0fb36195e1cbfd921110517
maosiy <58429563+maosiy@users.noreply.github.com>
2025-04-29 18:15:14 +0800

    Update text.cpp

--- text.cpp
+++ text.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 int main() 
 {    
-    std::cout << "Hello Git!" << std::endl;    
+    std::cout << "Hello Git!" << std::endl;
+    std::cout << "this is a new change" << std::endl;
     return 0;}
